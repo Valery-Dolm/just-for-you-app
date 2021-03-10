24d03bf2e0d9916203cdbba56aa18980b7f96c06
Valery Dolm <valery.pisannyh@gmail.com>
2021-03-10 09:18:28 +0300

    Added some changes

---
+++ justforyou/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+import "./index.css";
+
+
+export default class App extends Component {
+  render () {
+    return (
+    <div className="main">
+      Main
+    </div>
+    )
+    
+  }
+  
+}
+
+
